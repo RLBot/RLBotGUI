88656787da96250d5c67b2602a95b6ec32e64d75
Tyler Arehart <tarehart@gmail.com>
2019-01-02 12:22:42 -0500

    Adding Vue Material and drag-n-drop teams.

--- gui.py
+++ gui.py
@@ -1,24 +1,55 @@
+import multiprocessing
+
 import eel
+from rlbot.parsing.directory_scanner import scan_directory_for_bot_configs
 
 sm = None
 
-@eel.expose
-def startMatch():
+
+def startMatchHelper(configuration):
     from rlbot.setup_manager import SetupManager
+    global sm
     sm = SetupManager()
-    sm.startup()
+    print('connecting to game')
+    sm.connect_to_game()
+    print('loading config')
     sm.load_config()
-    sm.init_ball_prediction()
+    print('launching ball prediction')
+    sm.launch_ball_prediction()
+    print('launching quick chat')
+    # sm.launch_quick_chat_manager()
+    print('launching bot processes')
     sm.launch_bot_processes()
-    sm.run()
+    print('starting match')
+    sm.start_match()
+    print('starting infinite loop')
+    sm.infinite_loop()
+
 
 @eel.expose
-def killBots(self):
+def startMatch(configuration):
+    print(configuration)  # TODO: use this configuration
+
+    # TODO: figure out how to run the setup manager without blocking the eel process.
+    # See the Asynchronous Python section here: https://github.com/ChrisKnott/Eel
+    # process = multiprocessing.Process(target=startMatchHelper, args=(configuration))
+    # process.start()
+    eel.spawn(startMatchHelper(configuration))
+
+
+@eel.expose
+def killBots():
     if sm is not None:
         sm.shut_down(time_limit=5, kill_all_pids=True)
     else:
         print("There gotta be some setup manager already")
 
 
+@eel.expose
+def scanForBots(directory):
+    return [{'name': bundle.name, 'image': 'imgs/rlbot.png', 'path': bundle.config_path}
+            for bundle in scan_directory_for_bot_configs(directory)]
+
+
 eel.init('gui')
-eel.start('main.html', size=(880, 660), block=True)
+eel.start('main.html', size=(880, 660))
--- gui/js/main.js
+++ gui/js/main.js
@@ -2,3 +2,30 @@ eel.expose(PythonPrint);
 function PythonPrint(message) {
     console.log("Python: "+message);
 }
+
+Vue.use(VueMaterial.default);
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        botPool: [
+            {'name': 'Psyonix Bot', 'image': 'imgs/psyonix.png'},
+            {'name': 'Human', 'image': 'imgs/human.png'}
+        ],
+        blueTeam: [],
+        orangeTeam: [],
+    },
+    methods: {
+        startMatch: function (event) {
+            eel.startMatch({'blue': this.blueTeam, 'orange': this.orangeTeam})
+        }
+    }
+});
+
+eel.scanForBots('.')(botsReceived);
+
+function botsReceived(bots) {
+    app.botPool = app.botPool.concat(bots);
+    console.log(app.botPool);
+}
