0a930f1733fe51dd72a810561dfd6789d1356557
Dominik Schmid <dominikschmid93@gmail.com>
2019-03-24 16:10:22 +1100

    Fix creating bots in other drives than $tmp (#26)

--- rlbot_gui/bot_management/bot_creation.py
+++ rlbot_gui/bot_management/bot_creation.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from shutil import move
 import configparser
 import os
 import string
@@ -15,37 +17,38 @@ def convert_to_filename(text):
     """
     import unicodedata
     normalized = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode()
-    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    valid_chars = f'-_.() {string.ascii_letters}{string.digits}'
     filename = ''.join(c for c in normalized if c in valid_chars)
     filename = filename.replace(' ', '_')  # Replace spaces with underscores
     return filename
 
 
 def bootstrap_python_bot(bot_name, directory):
-    bot_directory = directory or "."
     sanitized_name = convert_to_filename(bot_name)
+    bot_directory = Path(directory or '.')
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        print('created temporary directory', tmpdirname)
+        tmpdir = Path(tmpdirname)
+        print('created temporary directory', tmpdir)
 
         download_and_extract_zip(
-            download_url="https://github.com/RLBot/RLBotPythonExample/archive/master.zip",
-            local_zip_path=f"{tmpdirname}/RLBotPythonExample.zip", local_folder_path=tmpdirname)
+            download_url='https://github.com/RLBot/RLBotPythonExample/archive/master.zip',
+            local_zip_path=tmpdir / 'RLBotPythonExample.zip', local_folder_path=tmpdir)
 
         try:
-            os.rename(f"{tmpdirname}/RLBotPythonExample-master", f"{bot_directory}/{sanitized_name}")
+            move(tmpdir / 'RLBotPythonExample-master', bot_directory / sanitized_name)
         except FileExistsError:
             return {'error': f'There is already a bot named {sanitized_name}, please choose a different name!'}
 
     # Choose appropriate file names based on the bot name
-    code_dir = f"{bot_directory}/{sanitized_name}/{sanitized_name}"
-    python_file = f"{code_dir}/{sanitized_name}.py"
-    config_file = f"{code_dir}/{sanitized_name}.cfg"
+    code_dir = bot_directory / sanitized_name / sanitized_name
+    python_file = code_dir / f'{sanitized_name}.py'
+    config_file = code_dir / f'{sanitized_name}.cfg'
 
     # We're making some big assumptions here that the file structure / names in RLBotPythonExample will not change.
-    os.rename(f"{bot_directory}/{sanitized_name}/python_example/", code_dir)
-    os.rename(f"{code_dir}/python_example.py", python_file)
-    os.rename(f"{code_dir}/python_example.cfg", config_file)
+    move(bot_directory / sanitized_name / 'python_example', code_dir)
+    move(code_dir / 'python_example.py', python_file)
+    move(code_dir / 'python_example.cfg', config_file)
 
     # Update the config file to point to the renamed files, and show the correct bot name.
 
@@ -79,8 +82,8 @@ def bootstrap_python_bot(bot_name, directory):
     agent_config = BaseAgent.base_create_agent_configurations()
     agent_config.parse_file(raw_bot_config)
     agent_config.set_value(BOT_CONFIG_MODULE_HEADER, BOT_NAME_KEY, bot_name)
-    agent_config.set_value(BOT_CONFIG_MODULE_HEADER, PYTHON_FILE_KEY, f"{sanitized_name}.py")
-    with open(config_file, "w", encoding='utf8') as f:
+    agent_config.set_value(BOT_CONFIG_MODULE_HEADER, PYTHON_FILE_KEY, f'{sanitized_name}.py')
+    with open(config_file, 'w', encoding='utf8') as f:
         f.write(str(agent_config))
 
     # This is intended to open the example python file in the default system editor for .py files.
--- rlbot_gui/bot_management/downloader.py
+++ rlbot_gui/bot_management/downloader.py
@@ -1,8 +1,9 @@
+from pathlib import Path
 import urllib
 import zipfile
 
 
-def download_and_extract_zip(download_url, local_zip_path, local_folder_path):
+def download_and_extract_zip(download_url: str, local_zip_path: Path, local_folder_path: Path):
     urllib.request.urlretrieve(download_url, local_zip_path)
 
     with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
