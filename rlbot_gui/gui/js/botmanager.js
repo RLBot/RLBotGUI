afedc4ffcee43f4da683b7907ee138f99b5eb3d4
Tyler Arehart <tarehart@gmail.com>
2019-08-01 21:35:17 -0700

    Converting the rlbot hub to vue, mostly.

--- rlbot_gui/gui/js/botmanager.js
+++ rlbot_gui/gui/js/botmanager.js
@@ -1,4 +1,28 @@
-var globalJson
+Vue.use(VueMaterial.default);
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        repos: [],
+        showProgressSpinner: false,
+    },
+    created: function () {
+        downloadRepos();
+    },
+    methods: {
+    	downloadBot: function (repo) {
+            eel.download_bot(repo.url, repo.name)(function() {
+				repo.is_installed = true;
+			})
+		},
+        deleteBot: function (repo) {
+            eel.delete_bot(repo.name)(function() {
+				repo.is_installed = false;
+            })
+        }
+	}
+});
+
 
 async function downloadRepos() {
 	await fetch('https://raw.githubusercontent.com/ard1998/RLBot-repos/master/trusted.json')
@@ -6,7 +30,7 @@ async function downloadRepos() {
 			return response.json();
 		})
 		.then(function(myJson) {
-			globalJson = myJson;
+			app.repos = myJson.repos;
 			addPackageData(myJson);
 		});
 }
@@ -17,7 +41,7 @@ async function addPackageData(json) {
 
 		var urlPart = "";
 
-		if (urlPart.indexOf("tree")==-1) {
+		if (urlPart.indexOf("tree") === -1) {
 			urlPart = repo.url.substr(18)+"/master";
 		}
 		else {
@@ -26,133 +50,16 @@ async function addPackageData(json) {
 			var branch = part.substr(part.indexOf("/tree")+5);
 			urlPart = link + branch;
 		}
-		var url = 'https://raw.githubusercontent.com' + urlPart + '/' + repo.name + '/botpackage.json'
+		var url = 'https://raw.githubusercontent.com' + urlPart + '/' + repo.name + '/botpackage.json';
 		await fetch(url)
 			.then(function(response) {
 				return response.json();
 			})
 			.then(function(myJson) {
-				globalJson.repos[index].testedFrameworkVersion = myJson.testedFrameworkVersion;
-				globalJson.repos[index].version = myJson.version;
-				globalJson.repos[index].categories = myJson.categories;
-				globalJson.repos[index].gamemodes = myJson.gamemodes;
-				globalJson = json;
+				app.repos[index].testedFrameworkVersion = myJson.testedFrameworkVersion;
+				app.repos[index].version = myJson.version;
+				app.repos[index].categories = myJson.categories;
+				app.repos[index].gamemodes = myJson.gamemodes;
 			});
 	}
-
-	displayRepoData();
-
-}
-
-async function displayRepoData() {
-	document.getElementById('main').innerHTML = '';
-	//make bot ui elements out of json data
-	for (var i = 0; i < globalJson.repos.length; i++) {
-		repo = globalJson.repos[i];
-		display = true;
-
-		//fill gamemode filters
-		var gamemodeFilters = [];
-		for (var j = document.getElementsByName('gamemode').length - 1; j >= 0; j--) {
-			if (document.getElementsByName('gamemode')[j].checked) {
-				arrLen = gamemodeFilters.length;
-				gamemodeFilters[arrLen] = document.getElementsByName('gamemode')[j].value;
-			}
-		}
-
-		var gamemodeNotFoundCount = 0;
-		if (gamemodeFilters.length != 0) {
-			//if any active gamemode gamemodeFilters, apply them
-			var gamemodeNotFoundCount = gamemodeFilters.length;
-			for (var gamemodeFilterIndex = gamemodeFilters.length - 1; gamemodeFilterIndex >= 0; gamemodeFilterIndex--) {
-				for (var j = repo.gamemodes.length - 1; j >= 0; j--) {
-					if (repo.gamemodes[j].name.toLowerCase() == gamemodeFilters[gamemodeFilterIndex].toLowerCase()) {
-						--gamemodeNotFoundCount;
-					}
-				}
-			}
-		}
-		if (gamemodeNotFoundCount != 0 || repo.name.toLowerCase().indexOf(document.getElementById('searchBotName').value.toLowerCase())==-1) {
-			display = false
-		}
-
-		if (display) {
-			gamemodeString = '';
-			for (var index = repo.gamemodes.length - 1; index >= 0; index--) {
-				gamemodeString+=repo.gamemodes[index].name;
-				if (index!=0) {
-					gamemodeString += ', ';
-				}
-			}
-
-			categoryString = '';
-			for (var index = repo.categories.length - 1; index >= 0; index--) {
-				categoryString+=repo.categories[index].name;
-				if (index!=0) {
-					categoryString += ', ';
-				}
-			}
-
-
-
-			//Filling in template and inserting it
-			var html = '';
-
-			html +=	"<div class=\"md-card settings-card md-theme-default\">";
-			html +=		"<div class=\"md-card-header\">";
-			html += 		"<div class=\"md-title\">"+repo.name+"</div>";
-			html +=		"</div>";
-			html +=		"<div class=\"md-card-content\">";
-			html += 		"<div class=\"center-flex\">";
-			html += 			"<div class=\"md-layout md-gutter\">";
-			html += 			    "<div class=\"md-layout-item\"><label for=\"gamemodes-0\">gamemode</label>";
-			html += 					"<p id=\"gamemodes-0\" class=\"managerInfo\">"+gamemodeString+"</p>";
-			html += 			    "</div>";
-			html += 			    "<div class=\"md-layout-item\"><label for=\"type-0\">Category</label>";
-			html += 					"<p id=\"type-0\" class=\"managerInfo\">"+categoryString+"</p>";
-			html += 			    "</div>";
-			html += 			"</div> ";
-			html += 			"<span style=\"flex-grow: 1;\"></span> ";
-			html += 			"<button type=\"button\" class=\"md-button md-primary md-raised md-theme-default\">";
-			html += 				"<div id=\"button"+i+"\" class=\"md-ripple\">";
-
-			if (await eel.is_bot_installed(repo.name)()) {
-				config = JSON.parse(await eel.get_bot_packaging(repo.name)());
-				if (config.version==repo.version) {
-					html +=				"<div class=\"md-button-content\" onclick=\"eel.delete_bot('"+repo.name+"')(buttonReplaceToDownload("+i+", '"+repo.name+"', '"+repo.url+"'))\">Delete</div>";
-				}
-				else {
-					html +=				"<div class=\"md-button-content\" onclick=\"eel.delete_bot('"+repo.name+"'); eel.download_bot('"+repo.url+"', '"+repo.name+"')(buttonReplaceToDelete("+i+", '"+repo.name+"', '"+repo.url+"'))\">Update</div>";	
-				}
-			}
-			else {
-				html +=					"<div class=\"md-button-content\" onclick=\"eel.download_bot('"+repo.url+"', '"+repo.name+"')(buttonReplaceToDelete("+i+", '"+repo.name+"', '"+repo.url+"'))\">Download</div>";	
-			}
-			html += 				"</div>"
-			html += 			"</button> ";
-			/*html += 			"<button type=\"button\" class=\"md-button md-raised md-theme-default\">";
-			html += 				"<div class=\"md-ripple\">";
-			html += 					"<div class=\"md-button-content\">More info</div>";
-			html += 				"</div>";
-			html += 			"</button>";
-			html += 			"<button type=\"button\" class=\"md-button md-raised md-theme-default\">";
-			html += 				"<div class=\"md-ripple\">";
-			html += 					"<div class=\"md-button-content\">Configure bot</div>";
-			html += 				"</div>";
-			html += 			"</button>";*/
-			html += 		"</div>";
-			html +=		"</div>";
-			html +=	"</div>";
-
-			document.getElementById('main').innerHTML += html;
-		}
-	}
-}
-
-function buttonReplaceToDownload(index, name, url) {
-	document.getElementById('button'+index).innerHTML = "<div class=\"md-button-content\" onclick=\"eel.download_bot('"+url+"', '"+repo.name+"')(buttonReplaceToDelete("+index+", '"+name+"', '"+url+"'))\">Download</div>";	
-}
-
-function buttonReplaceToDelete(index, name, url) {
-	document.getElementById('button'+index).innerHTML = "<div class=\"md-button-content\" onclick=\"eel.delete_bot('"+name+"')(buttonReplaceToDownload("+index+", '"+name+"', '"+url+"'))\">Delete</div>";
 }
