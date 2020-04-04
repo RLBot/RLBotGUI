0c22277e49df02dc5eeb88b555c5aa091ea88105
Tyler <tarehart@gmail.com>
2020-04-04 14:17:30 -0700

    Upgrade script to upgrade the upgrade script. (#77)

---
+++ rlbot_gui/upgrade/upgrade_replacer.py
@@ -0,0 +1,21 @@
+import shutil
+from pathlib import Path
+
+from env.Lib import os
+
+
+def replace_upgrade_file():
+    """
+    RLBotGUI installations on windows generally have a file at ./pynsist_helpers/upgrade.py which is
+    NOT part of the rlbot_gui python package. It lives outside the package for the sake of upgrading
+    the package. However, sometimes we wish to push updates to upgrade.py without requiring users to
+    manually reinstall. This function takes a version of the upgrade script vended inside rlbot_gui
+    and copies it to the external location.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    fresh_upgrade_file = Path(os.path.join(dir_path, 'upgrade_script.py'))
+    existing_upgrade_file = Path('./pynsist_helpers/upgrade.py')
+
+    if existing_upgrade_file.exists():
+        shutil.copyfile(fresh_upgrade_file, existing_upgrade_file)
+        print(f"Successfully replaced {existing_upgrade_file.absolute()}.")
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from rlbot_gui import gui
+from rlbot_gui.upgrade.upgrade_replacer import replace_upgrade_file
 
 # Insert the pkgs directory into the python path. This is necessary when
 # running the pynsist installed version.
@@ -10,4 +11,5 @@ pkgdir = os.path.join(scriptdir, 'pkgs')
 sys.path.insert(0, pkgdir)
 
 if __name__ == '__main__':
+    replace_upgrade_file()
     gui.start()
