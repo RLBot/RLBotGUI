f336b2e335fa664cfdf1329a06a5671f068c09db
Darxeal <jira.pihrt@volny.cz>
2020-04-12 01:13:05 +0200

    Remove Botpack LFS downloading (#78)

--- rlbot_gui/bot_management/downloader.py
+++ rlbot_gui/bot_management/downloader.py
@@ -18,8 +18,8 @@ def download_and_extract_zip(download_url: str, local_folder_path: Path,
     """
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        downloadedZip = os.path.join(tmpdirname, 'downloaded.zip')
-        urllib.request.urlretrieve(download_url, downloadedZip, progress_callback)
+        downloaded_zip_path = os.path.join(tmpdirname, 'downloaded.zip')
+        urllib.request.urlretrieve(download_url, downloaded_zip_path, progress_callback)
 
         if clobber and os.path.exists(local_folder_path):
             rmtree(local_folder_path)
@@ -27,7 +27,7 @@ def download_and_extract_zip(download_url: str, local_folder_path: Path,
         if unzip_callback:
             unzip_callback()
 
-        with zipfile.ZipFile(downloadedZip, 'r') as zip_ref:
+        with zipfile.ZipFile(downloaded_zip_path, 'r') as zip_ref:
             zip_ref.extractall(local_folder_path)
 
 
@@ -55,18 +55,13 @@ class BotpackDownloader:
     """
 
     PROGRESSBAR_UPDATE_INTERVAL = 0.1  # How often to update the progress bar (seconds)
-    ZIP_PORTION = 0.6  # How much of total progress should the zip file take
 
     def __init__(self):
         self.status = ''
-        self.current_progress = 0
         self.total_progress = 0
 
         self.estimated_zip_size = 0
-        self.current_file_bytes_downloaded = 0
-        self.lfs_file_count = 0
-        self.lfs_files_completed = 0
-        self.one_lfs_file_portion = 0
+        self.downloaded_bytes = 0
         self.last_progressbar_update_time = 0
 
     def update_progressbar_and_status(self):
@@ -76,77 +71,39 @@ class BotpackDownloader:
         if now > self.last_progressbar_update_time + self.PROGRESSBAR_UPDATE_INTERVAL:
             self.last_progressbar_update_time = now
 
-            total_progress_percent = self.total_progress * 100
-            current_progress_percent = int(self.current_progress * 100)
-            status = f'{self.status} ({current_progress_percent}%)'
+            total_progress_percent = int(self.total_progress * 100)
+            status = f'{self.status} ({total_progress_percent}%)'
 
             eel.updateDownloadProgress(total_progress_percent, status)
 
     def zip_download_callback(self, block_count, block_size, _):
-        self.current_file_bytes_downloaded += block_size
-        self.current_progress = min(self.current_file_bytes_downloaded / self.estimated_zip_size, 1.0)
-        self.total_progress = self.current_progress * self.ZIP_PORTION
+        self.downloaded_bytes += block_size
+        self.total_progress = min(self.downloaded_bytes / self.estimated_zip_size, 1.0)
         self.update_progressbar_and_status()
 
     def unzip_callback(self):
-        eel.updateDownloadProgress(int(self.ZIP_PORTION * 100), 'Extracting ZIP file')
-
-    def lfs_download_callback(self, block_count, block_size, total_size):
-        self.current_file_bytes_downloaded += block_size
-        self.current_progress = self.current_file_bytes_downloaded / total_size
-        prev_files_progress = self.ZIP_PORTION + self.one_lfs_file_portion * self.lfs_files_completed
-        self.total_progress = prev_files_progress + self.current_progress * self.one_lfs_file_portion
-        self.update_progressbar_and_status()
+        eel.updateDownloadProgress(100, 'Extracting ZIP file')
 
     def download(self, repo_owner: str, repo_name: str, branch_name: str, checkout_folder: Path):
         repo_full_name = repo_owner + '/' + repo_name
         repo_url = 'https://github.com/' + repo_full_name
-        repo_extraction_name = f'/{repo_name}-{branch_name}/'
 
         self.status = f'Downloading {repo_full_name}-{branch_name}'
         print(self.status)
-        self.current_progress = 0
         self.total_progress = 0
 
         # Unfortunately we can't know the size of the zip file before downloading it,
         # so we have to get the size from the GitHub API.
-        self.estimated_zip_size = get_repo_size(repo_full_name) * 0.9
+        self.estimated_zip_size = get_repo_size(repo_full_name) * 0.5
 
         # If we fail to get the repo size, set it to a fallback value,
         # so the progress bar will show atleast some progress.
-        # Let's assume the zip file is around 50 MB.
+        # Let's assume the zip file is around 15 MB.
         if self.estimated_zip_size == 0:
-            self.estimated_zip_size = 50_000_000
+            self.estimated_zip_size = 15_000_000
 
-        # Download the most of the files eg. https://github.com/RLBot/RLBotPack/archive/master.zip
-        download_and_extract_zip(
-            download_url=repo_url + '/archive/' + branch_name + '.zip',
-            local_folder_path=checkout_folder, clobber=True,
-            progress_callback=self.zip_download_callback,
-            unzip_callback=self.unzip_callback)
-
-        # Get paths of all LFS files we need to download from the .gitattributes file.
-        lfs_file_paths = []
-        gitattributes_file = open(checkout_folder + repo_extraction_name + '.gitattributes')
-        for line in gitattributes_file:
-            if line.split(' ')[1].endswith('=lfs'):
-                lfs_file_paths.append(line.split(' ')[0])
-        gitattributes_file.close()
-
-        self.lfs_file_count = len(lfs_file_paths)
-        # How much of total progress does one LFS file take:
-        self.one_lfs_file_portion = (1.0 - self.ZIP_PORTION) / self.lfs_file_count
-
-        # Download all LFS files.
-        for lfs_file_path in lfs_file_paths:
-            self.current_progress = 0
-            self.current_file_bytes_downloaded = 0
-            self.status = 'Downloading ' + lfs_file_path
-            print(self.status)
-
-            urllib.request.urlretrieve(
-                repo_url + '/raw/' + branch_name + '/' + lfs_file_path,
-                checkout_folder + repo_extraction_name + lfs_file_path,
-                self.lfs_download_callback)
-
-            self.lfs_files_completed += 1
+        download_and_extract_zip(download_url=repo_url + '/archive/' + branch_name + '.zip',
+                                 local_folder_path=checkout_folder,
+                                 clobber=True,
+                                 progress_callback=self.zip_download_callback,
+                                 unzip_callback=self.unzip_callback)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-__version__ = '0.0.53'
+__version__ = '0.0.54'
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
