f5270518e5d0f94cf94943d7668be46d6631af74
Tyler Arehart <tarehart@gmail.com>
2020-06-11 16:42:36 -0700

    Making the install script more tolerant of spaces in path and launching outside C:

--- alternative-install/RLBotGUI.bat
+++ alternative-install/RLBotGUI.bat
@@ -2,6 +2,10 @@
 
 echo Installing RLBotGUI if necessary, then launching!
 
+
+if not exist "%LocalAppData%\RLBotGUIX" mkdir "%LocalAppData%\RLBotGUIX"
+pushd "%LocalAppData%\RLBotGUIX"
+
 py -3.7 --version
 
 rem If python 3.7 is not installed, then the above py command will cause a non-zero error level.
@@ -11,8 +15,7 @@ if %ERRORLEVEL% GTR 0 (
   rem Download a python installer from python.org.
 
   if not exist python-install.exe (
-    mkdir %LocalAppData%\RLBotGUIX\
-    powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -OutFile %LocalAppData%\RLBotGUIX\python-installer.exe"
+    powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -OutFile .\python-installer.exe"
   )
 
   rem Go ahead and install the specific version of python we want.
@@ -20,17 +23,17 @@ if %ERRORLEVEL% GTR 0 (
   rem If the user already has 3.7 installed but the py command was not available or could not find it,
   rem the installation will be modified, which will probably not cause trouble for anyone.
 
-  %LocalAppData%\RLBotGUIX\python-installer.exe /passive InstallAllUsers=0 Shortcuts=0 Include_doc=0 Include_dev=0 Include_launcher=1 InstallLauncherAllUsers=0 PrependPath=0
+  .\python-installer.exe /passive InstallAllUsers=0 Shortcuts=0 Include_doc=0 Include_dev=0 Include_launcher=1 InstallLauncherAllUsers=0 PrependPath=0
 )
 
 rem Create a virtual environment which will isolate our package installations from any
 rem existing python installation that the user may have.
 
-if not exist %LocalAppData%\RLBotGUIX\venv\Scripts\python.exe (
-  py -3.7 -m venv %LocalAppData%\RLBotGUIX\venv
+if not exist .\RLBotGUIX\venv\Scripts\python.exe (
+  py -3.7 -m venv .\venv
 )
 
-set rlbotpy=%LocalAppData%\RLBotGUIX\venv\Scripts\python.exe
+set rlbotpy=.\venv\Scripts\python.exe
 
 rem Install and / or upgrade RLBot components
 
@@ -40,5 +43,5 @@ rem Install and / or upgrade RLBot components
 %rlbotpy% -m pip install --upgrade rlbot_gui rlbot
 
 rem Launch the GUI
-cd %LocalAppData%\RLBotGUIX
+
 %rlbotpy% -c "from rlbot_gui import gui; gui.start()"
