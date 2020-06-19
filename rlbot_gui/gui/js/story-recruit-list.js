5c8950f6fe8f49a8d8817c579d1eff614c1fc675
Azeem Bande-Ali <me@azeemba.com>
2020-06-19 00:45:43 -0400

    Add List to Show Recruitable Players

---
+++ rlbot_gui/gui/js/story-recruit-list.js
@@ -0,0 +1,22 @@
+
+export default {
+    name: 'story-recruit-list',
+    props: {
+        recruitables: Array
+    },
+    template: /*html*/ `
+    <b-list-group>
+        <b-list-group-item
+            v-for="recruit in recruitables"
+            class="d-flex justify-content-between align-items-center"
+            v-bind:variant="recruit.recruited ? 'success' : 'default'">
+            {{recruit.name}}
+            <b-button v-if="!recruit.recruited"
+                variant="success"
+                @click="$emit('recruit', recruit.id)">
+                Recruit
+            </b-button>
+        </b-list-group-item>
+    </b-list-group>
+    `,
+}
