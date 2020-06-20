56064e557d47cdf9050c43b213d4c18e3d6a49d9
VirxEC <virx@virxcase.dev>
2020-06-20 12:30:33 -0400

    New Linux install!

---
+++ linux-install/README.md
@@ -0,0 +1,27 @@
+# RLBotGUI Ubuntu (Linux) Install
+
+## First-time installation
+
+There are a few steps to install this on Linux.
+
+1. Download `RLBotGUI.sh`
+2. `chmod +x /path/to/RLBotGUI.sh`
+3. `./RLBotGUI`
+4. You will be asked to provide your password. The RLBotGUI requires sudo in order to run:
+
+   - `sudo apt-get update`
+   - `sudo apt-get install python3.8` NOTE: If you're using Ubuntu 20.04, This won't be run as you'll already have Python 3.8 installed.
+   - `sudo apt-get install python3.8-venv`
+   - `sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py`
+   - `sudo rm get-pip.py`
+
+   We won't do anything else with the sudo permission you give us.
+
+5. Once the shell script does it's thing, the RLBotGUI will open! You can now take `RLBotGUI.sh` and put it where every in your system you want. The RLBotGUI has been installed, and you won't need to do it again. (Or at least for a while.)
+
+## Opening RLBotGUI after installation
+
+1. Where ever you've put `RLBotGUI.sh`, run it using `path/to/RLBotGUI.sh`
+2. Unlike the first time you ran the script, you won't be prompted for you password. This is because it isn't needed.
+3. The shell script will now update all dependicies if needed (`pip`, `wheel`, `setuptools`, `eel`, `rlbot_gui` and `rlbot`)
+4. The RLBotGUI will then launch.
---
+++ linux-install/RLBotGUI.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set +v
+
+echo "Installing RLBotGUI is necessary, then launching!"
+echo ""
+
+if [ ! -d "$HOME/.RLBotGUI" ]
+then
+    sudo apt-get update
+    mkdir "$HOME/.RLBotGUI"
+fi
+
+pushd "$HOME/.RLBotGUI"
+
+
+# If any version of Python 3.8 is not installed, install Python 3.8
+
+parsedVersion=$(echo "${version//./}")
+if [[ "$parsedVersion" -gt "380" ]]
+then
+    echo "Invalid Python install. Installing Python 3.8..."
+    sudo apt-get install python3.8
+else
+    echo "Detected valid Python install"
+fi
+
+# Check if the virtual environment exists
+
+if [ ! -e "$HOME/.RLBotGUI/env/bin/activate" ]
+then
+
+    # Check if the user has python3.8-venv installed. If they don't, then install it
+    
+    python3.8 -c "import venv"
+
+    if [ $? -gt 0 ]
+    then
+        echo "Installing the Python 3.8 Virtual Environment"
+        sudo apt-get install python3.8-venv
+    fi
+
+    # Create the virutal environment
+    # There's currently a bug in Ubuntu, so we must create the venv without pip and then manually install it
+
+    echo "Creating the Python 3.8 Virtual Environment"
+
+    python3.8 -m venv --without-pip env
+
+    # Enter the virtual environment and install pip, wheel and setuptools
+    source ./env/bin/activate
+    sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+    python get-pip.py
+
+    # Pip is now installed, so we can remove get-pip.py as it's no longer needed
+    sudo rm get-pip.py
+
+    pip install rlbot_gui rlbot
+else
+    # Enter the virtual environment and upgrade all packages
+    echo "Activating virtual environment"
+    source ./env/bin/activate
+    echo "Updating packages"
+    pip install --upgrade pip wheel setuptools eel rlbot_gui rlbot
+fi
+
+# Launch the GUI
+
+python -c "from rlbot_gui import gui; gui.start()"
