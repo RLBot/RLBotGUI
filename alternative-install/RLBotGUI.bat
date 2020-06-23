fdbba8240585f72f467fe4f2a7a50ddf669323e5
Tyler Arehart <tarehart@gmail.com>
2020-06-23 02:48:33 -0700

    Making the install script refresh environment variables after installing python.

--- alternative-install/RLBotGUI.bat
+++ alternative-install/RLBotGUI.bat
@@ -12,7 +12,7 @@ rem If python 3.7 is not installed, then the above py command will cause a non-z
 
 if %ERRORLEVEL% GTR 0 (
 
-  rem Download a python installer from python.org.
+  echo Looks like we don't have python 3.7, will download and install from python.org...
 
   if not exist python-install.exe (
     powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -OutFile .\python-installer.exe"
@@ -29,20 +29,25 @@ if %ERRORLEVEL% GTR 0 (
 rem Create a virtual environment which will isolate our package installations from any
 rem existing python installation that the user may have.
 
-if not exist .\RLBotGUIX\venv\Scripts\python.exe (
+if not exist .\venv\Scripts\python.exe (
+  rem if we just installed python for the first time then py is probably still not on PATH.
+  rem Make sure the environment variables are up-to-date.
+  call %~dp0\RefreshEnv.cmd
+
+  echo Creating python virtual environment just for RLBot...
   py -3.7 -m venv .\venv
 )
 
 set rlbotpy=.\venv\Scripts\python.exe
 
-rem Install and / or upgrade RLBot components
+echo Installing / upgrading RLBot components...
 
 %rlbotpy% -m pip install --upgrade pip
 %rlbotpy% -m pip install wheel
 %rlbotpy% -m pip install eel
 %rlbotpy% -m pip install --upgrade rlbot_gui rlbot
 
-rem Launch the GUI
+echo Launching RLBotGUI...
 
 %rlbotpy% -c "from rlbot_gui import gui; gui.start()"
 
