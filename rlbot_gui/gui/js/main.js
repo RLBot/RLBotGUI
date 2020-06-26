5a7d2261ba167b70c36c8eb49006174f924e7cae
L0laapk3 <L0laapk3@gmail.com>
2020-06-26 21:11:35 +0200

    cherry-pick 888fc5e: Close GUI when terminal is closed.

--- rlbot_gui/gui/js/main.js
+++ rlbot_gui/gui/js/main.js
@@ -3,6 +3,28 @@ function PythonPrint(message) {
     console.log("Python: "+message);
 }
 
+<<<<<<< HEAD
+=======
+
+// eel does not provide an API for this. Lets just edit the browser functions it uses. Improvise, adapt, overcome.
+const _WebSocket = window.WebSocket;
+window.WebSocket = function() {
+    const ws = new _WebSocket(...arguments);
+    ws.onclose = window.close.bind(window);
+    return ws;
+};
+
+
+// looks like if you want to expose a function inside a vue component,
+// you first have to expose it's signature like this
+eel.expose(updateDownloadProgress);
+function updateDownloadProgress(progress, status) {}
+// otherwise python will think it doesn't exist
+// you can then expose the actual function later
+
+// Vue.use(VueMaterial.default);
+
+>>>>>>> 888fc5e... Close GUI when terminal is closed.
 import Main from './main-vue.js'
 import Sandbox from './sandbox-vue.js'
 import Story from './story-mode.js'
