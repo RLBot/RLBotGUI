015251821d475fb54152663eca4311f26340873d
Jiří Pihrt <jira.pihrt@volny.cz>
2020-06-27 17:55:25 +0200

    Get rid of the signature exposing since we aren't using VueLoader anymore

--- rlbot_gui/gui/js/main.js
+++ rlbot_gui/gui/js/main.js
@@ -3,15 +3,6 @@ function PythonPrint(message) {
     console.log("Python: "+message);
 }
 
-// looks like if you want to expose a function inside a vue component,
-// you first have to expose it's signature like this
-eel.expose(updateDownloadProgress);
-function updateDownloadProgress(progress, status) {}
-// otherwise python will think it doesn't exist
-// you can then expose the actual function later
-
-// Vue.use(VueMaterial.default);
-
 import Main from './main-vue.js'
 import Sandbox from './sandbox-vue.js'
 import Story from './story-mode.js'
