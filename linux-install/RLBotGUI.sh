d9a0cbdb2bbcada8aeece41220474bbbd2cf7b94
Virx <virx@virxcase.dev>
2020-06-30 10:45:35 -0400

    Update RLBotGUI.sh
    Instead of waiting to see if packages aren't installed, just install them. If they're already installed, then nothing will happen.
    Also, the script now runs garbage collection after installing the packages.
    More noticeable status messages from the script
    Smarter Python 3.7 installation detection

--- linux-install/RLBotGUI.sh
+++ linux-install/RLBotGUI.sh
@@ -13,15 +13,17 @@ fi
 
 pushd "$HOME/.RLBotGUI"
 
-
 # If any version of Python 3.7 is not installed, then install it
 
-parsedVersion=$(echo "${version//./}")
-if [[ "$parsedVersion" -gt "370" && "$parsedVersion" -lt "380" ]]
+python3.7 -V
+if [ $? -gt 0 ]
 then
-    echo "Detected valid Python install"
-else
-    echo "Invalid Python install. Installing Python 3.7..."
+	echo ""
+    echo "Invalid Python install. Installing Python 3.7, and possibly not present dependencies..."
+	echo ""
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt-get update
     sudo apt-get install python3.7
 fi
 
@@ -29,68 +31,57 @@ fi
 
 if [ ! -e "$HOME/.RLBotGUI/env/bin/activate" ]
 then
-
-    # Check if the user has python3.7-venv installed. If they don't, then install it
-    
-    python3.7 -c "import venv"
-
-    if [ $? -gt 0 ]
-    then
-        echo "Installing the Python 3.7 Virtual Environment"
-        sudo apt-get install python3.7-venv
-    fi
-
+	# Instead of waiting to see if these aren't installed, just install them. If they're already installed, then nothing will happen.
+	echo ""
+    echo "Installing build-essential, python3.7-venv, python3.7-dev, python3-distutils, and curl if they're needed"
+	echo ""
+    sudo apt-get install build-essential
+    sudo apt-get install python3.7-venv
+    sudo apt-get install python3.7-dev
+    sudo apt-get install python3-distutils
+    sudo apt-get install curl
+    sudo apt autoremove
+	
     # Create the virutal environment
     # There's currently a bug in Ubuntu, so we must create the venv without pip and then manually install it
 
+	echo ""
     echo "Creating the Python 3.7 Virtual Environment"
+	echo ""
 
     python3.7 -m venv --without-pip env
 
     # Enter the virtual environment
     source ./env/bin/activate
 
-    # Check if curl is installed and install it if needed
-    curl -V
-
-    if [ $? -gt 0 ]
-    then
-        echo "Installing cURL"
-        sudo apt-get install curl
-    fi
-
     # Install pip, wheel and setuptools
     sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
     python get-pip.py
-    if [ $? -gt 0 ]
-    then
-        echo "Installing distutils"
-        sudo apt-get install python3-distutils
-        python get-pip.py
-    fi
-
+	
     # Pip is now installed, so we can remove get-pip.py as it's no longer needed
     sudo rm get-pip.py
 
     # Install packages
+	
     pip install eel rlbot_gui rlbot
-    if [ $? -gt 0 ]
-    then
-        echo "Installing build-essential and python3.7-dev"
-		sudo apt-get install build-essential
-        sudo apt-get install python3.7-dev
-        pip install rlbot
-    fi
 
 else
     # Enter the virtual environment and upgrade all packages
+	echo ""
     echo "Activating virtual environment"
+	echo ""
     source ./env/bin/activate
+	
+	echo ""
     echo "Updating packages"
+	echo ""
     pip install --upgrade pip wheel setuptools eel rlbot_gui rlbot
 fi
 
 # Launch the GUI
 
+echo ""
+echo "Launching RLBotGUI"
+echo ""
 python -c "from rlbot_gui import gui; gui.start()"
