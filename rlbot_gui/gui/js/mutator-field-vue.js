99fc1c012774bfce8a7be8a1104ad2e8361e132c
Darxeal <jira.pihrt@volny.cz>
2020-10-12 19:45:40 +0200

    Highlight active mutators & badge with their count (#125)

--- rlbot_gui/gui/js/mutator-field-vue.js
+++ rlbot_gui/gui/js/mutator-field-vue.js
@@ -3,7 +3,9 @@ export default {
 	props: ['label', 'options', 'value'],
 	template: `
 	<b-form-group :label="label">
-		<b-form-select v-model="model" :id="id" v-on:change="$emit('input', $event)">
+		<b-form-select v-model="model" :id="id"v-on:change="$emit('input', $event)"
+			:class="{'highlighted-mutator-field': value != options[0]}"
+		>
 			<b-form-select-option v-for="opt in options" :key="opt" :value="opt">{{opt}}</b-form-select-option>
 		</b-form-select>
 	</b-form-group>
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-__version__ = '0.0.81'
+__version__ = '0.0.82'
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
