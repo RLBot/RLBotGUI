def1babf2156e479b5c98f27db8edba731acbd2d
Tyler Arehart <tarehart@gmail.com>
2022-05-07 16:36:55 -0700

    Adding input for RocketLeague.exe path for Epic launcher.

--- rlbot_gui/gui/js/launcher-preference-vue.js
+++ rlbot_gui/gui/js/launcher-preference-vue.js
@@ -9,6 +9,16 @@ export default {
 				<b-form-radio v-model="launcherSettings.preferred_launcher" name="launcher-radios" value="steam">Steam</b-form-radio>
 				<b-form-radio v-model="launcherSettings.preferred_launcher" name="launcher-radios" value="epic_only">Epic Games</b-form-radio>
 			</b-form-group>
+			<b-form-group 
+				label="Optional: Path to RocketLeague.exe for Epic Launcher" 
+				description="Only needed if it's not working automatically, only used by Epic launcher.">
+				<b-form-input
+					v-model="launcherSettings.rocket_league_exe_path"
+					trim
+					placeholder="C:\\Path\\To\\Your\\RocketLeague.exe"
+					:disabled="launcherSettings.preferred_launcher == 'steam'">
+				</b-form-input>
+			</b-form-group>
 		</div>
 		<b-button variant="primary" class="mt-3" @click="saveLauncherSettings()">Save</b-button>
 	</div>
