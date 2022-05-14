49d99fa51f2c2058e0a407bfb80355ac063e1bb8
Darxeal <jira.pihrt@volny.cz>
2022-05-15 01:17:45 +0200

    Display info and warning icons in team cards, show compatibility warning modal when such a bot is added to a team, refactoring and fixes

--- rlbot_gui/gui/js/team-card-vue.js
+++ rlbot_gui/gui/js/team-card-vue.js
@@ -1,20 +1,21 @@
+import RunnableCard from './runnable-card-vue.js'
+
 export default {
 	name: 'team-card',
+    components: {
+		'runnable-card': RunnableCard,
+	},
 	props: ['value', 'team-class'],
 	template: `
 		<b-card class="team-card md-elevation-8" :class="teamClass">
             <div class="team-label">
                 <slot></slot>
             </div>
-            <draggable v-model="team" class="team-entries" :options="{group:'bots'}">
-                <b-card class="bot-card draggable center-flex md-elevation-3" v-for="(bot, index) in team">
-                    <img v-if="!bot.logo" class="darkened" v-bind:src="bot.image">
-                    <img v-if="bot.logo" v-bind:src="bot.logo">
-                    <span class="bot-name">{{ bot.name }} <span v-if="bot.uniquePathSegment" class="unique-bot-identifier">({{ bot.uniquePathSegment }})</span></span>
-                    <b-button size="sm" variant="outline-danger" class="icon-button" @click="team.splice(index, 1)">
-                        <b-icon icon="x"></b-icon>
-                    </b-button>
-                </b-card>
+            <draggable v-model="team" class="team-entries" :options="{group:'bots'}" @change="$event.added ? $emit('botadded', $event.added.element) : 0">
+                <runnable-card
+                    v-for="(bot, index) in team" :runnable="bot" class="draggable"
+                    removable @remove="team.splice(index, 1)">
+                </runnable-card>
             </draggable>
         </b-card>
     `,
