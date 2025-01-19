8675e35dad1df3abb79723dad6b3154181346219
Nicolaj Østerby Jensen <noje@cs.aau.dk>
2025-01-19 16:54:09 +0100

    Bump version

--- rlbot_gui/gui.py
+++ rlbot_gui/gui.py
@@ -34,7 +34,7 @@ from rlbot_gui.match_runner.match_runner import hot_reload_bots, shut_down, star
 from rlbot_gui.match_runner.custom_maps import find_all_custom_maps
 from rlbot_gui.type_translation.packet_translation import convert_packet_to_dict
 from rlbot_gui.persistence.settings import load_settings, BOT_FOLDER_SETTINGS_KEY, MATCH_SETTINGS_KEY, \
-    LAUNCHER_SETTINGS_KEY, TEAM_SETTINGS_KEY, load_launcher_settings, launcher_preferences_from_map, FAVORITES_KEY
+    LAUNCHER_SETTINGS_KEY, TEAM_SETTINGS_KEY, FAVORITES_KEY, load_launcher_settings, launcher_preferences_from_map
 from rlbot import gateway_util
 
 #### LOAD JUST TO EXPOSE STORY_MODE
